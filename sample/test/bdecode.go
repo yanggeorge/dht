8ba0a03f24b1a6a5f723358b9ae8d45e08d8d31e
yanggeorge <alenym@qq.com>
2019-03-09 21:29:41 +0800

    implementation of bdecode

--- sample/test/bdecode.go
+++ sample/test/bdecode.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"container/list"
 	"errors"
 	"fmt"
 	"unicode"
@@ -17,11 +16,11 @@ func NewBDecode(arr []byte) BDecode {
 	return BDecode{arr, len(arr), 0}
 }
 
-func (b BDecode) Parse() (map[string]interface{}, error) {
+func (b *BDecode) Parse() (map[string]interface{}, error) {
 	return b.Dic()
 }
 
-func (b BDecode) Dic() (map[string]interface{}, error) {
+func (b *BDecode) Dic() (map[string]interface{}, error) {
 	c, err := b.peek(0)
 	if err != nil || c != 'd' {
 		return nil, fmt.Errorf("error")
@@ -31,7 +30,10 @@ func (b BDecode) Dic() (map[string]interface{}, error) {
 	var key string
 	var val interface{}
 	c, err = b.peek(0)
-	for err != nil && c != 'e' {
+	if err != nil {
+		return nil, fmt.Errorf("error")
+	}
+	for c != 'e' {
 		key, err = b.string()
 		if err != nil {
 			return nil, fmt.Errorf("error")
@@ -42,6 +44,9 @@ func (b BDecode) Dic() (map[string]interface{}, error) {
 		}
 		dic[key] = val
 		c, err = b.peek(0)
+		if err != nil {
+			return nil, fmt.Errorf("error")
+		}
 	}
 
 	c, err = b.next()
@@ -52,15 +57,15 @@ func (b BDecode) Dic() (map[string]interface{}, error) {
 	return dic, nil
 }
 
-func (b BDecode) peek(n int) (rune, error) {
-	if b.i+n+1 < b.n {
+func (b *BDecode) peek(n int) (rune, error) {
+	if b.i+n < b.n {
 		return rune(b.arr[b.i+n]), nil
 	}
 	return 0, errors.New("out of range")
 }
 
-func (b BDecode) next() (rune, error) {
-	if b.i+1 < b.n {
+func (b *BDecode) next() (rune, error) {
+	if b.i < b.n {
 		c := rune(b.arr[b.i])
 		b.i += 1
 		return c, nil
@@ -68,7 +73,7 @@ func (b BDecode) next() (rune, error) {
 	return 0, errors.New("out of range")
 }
 
-func (b BDecode) string() (string, error) {
+func (b *BDecode) string() (string, error) {
 	num, err := b.num()
 	if err != nil {
 		return "", fmt.Errorf("error")
@@ -79,11 +84,11 @@ func (b BDecode) string() (string, error) {
 	}
 
 	s := string(b.arr[b.i : b.i+num])
-
+	b.i += num
 	return s, nil
 }
 
-func (b BDecode) num() (int, error) {
+func (b *BDecode) num() (int, error) {
 	c, err := b.peek(0)
 	if err != nil {
 		return 0, fmt.Errorf("error")
@@ -102,41 +107,30 @@ func (b BDecode) num() (int, error) {
 	return num, nil
 }
 
-func (b BDecode) element() (interface{}, error) {
+func (b *BDecode) element() (interface{}, error) {
 	c, err := b.peek(0)
 	if err != nil {
 		return nil, fmt.Errorf("error")
 	}
 
-	switch c {
-	case 'i':
+	if c == 'i' {
 		return b.integer()
-	case 'd':
+	} else if c == 'd' {
 		return b.Dic()
-	case 'l':
+	} else if c == 'l' {
 		return b.list()
-	case '0':
-	case '1':
-	case '2':
-	case '3':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
-	case '9':
-		return b.string()
-	default:
+	} else {
+		if unicode.IsDigit(c) {
+			return b.string()
+		}
 		return nil, fmt.Errorf("error")
 	}
-
-	return nil, fmt.Errorf("error")
 }
 
-func (b BDecode) integer() (int, error) {
+func (b *BDecode) integer() (int, error) {
 	c, err := b.next()
 	if err != nil || c != 'i' {
-		return 0, fmt.Errorf("error")
+		return 0, err
 	}
 	num, err := b.num()
 	if err != nil {
@@ -150,8 +144,8 @@ func (b BDecode) integer() (int, error) {
 	return num, nil
 }
 
-func (b BDecode) list() (*list.List, error) {
-	lis := list.New()
+func (b *BDecode) list() ([]interface{}, error) {
+	var lis []interface{}
 
 	c, err := b.next()
 	if err != nil || c != 'l' {
@@ -167,7 +161,12 @@ func (b BDecode) list() (*list.List, error) {
 		if err != nil {
 			return lis, fmt.Errorf("error")
 		}
-		lis.PushBack(ele)
+		lis = append(lis, ele)
+		c, err = b.peek(0)
+		if err != nil {
+			return lis, fmt.Errorf("error")
+		}
 	}
+	b.next()
 	return lis, nil
 }
--- sample/test/bdecode_test.go
+++ sample/test/bdecode_test.go
@@ -2,18 +2,52 @@ package test
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
-func TestParse(t *testing.T) {
-	bs := make([]byte, 12)
-	bs = []byte("hello")
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+func TestString(t *testing.T) {
+	bs := []byte("5:hello")
 	b := NewBDecode(bs)
-	c, err := b.peek(12)
+	s, err := b.string()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	fmt.Println(c)
-	a := '0'
-	fmt.Println(int(a) - '0')
+	fmt.Println(s)
+}
+
+func TestInteger(t *testing.T) {
+	bs := []byte("i343e")
+	b := NewBDecode(bs)
+	i, err := b.integer()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(i)
+}
+
+func TestParse1(t *testing.T) {
+	bs := []byte("d4:spaml1:a1:bee")
+	b := NewBDecode(bs)
+	dic, e := b.Parse()
+	check(e)
+	fmt.Println(dic)
+}
+
+func TestParse(t *testing.T) {
+	path := "/Users/ym/tmp/venom.torrent"
+	dat, e := ioutil.ReadFile(path)
+	check(e)
+	fmt.Println(dat)
+	b := NewBDecode(dat)
+	dic, e := b.Parse()
+	check(e)
+	fmt.Println(dic)
 }
